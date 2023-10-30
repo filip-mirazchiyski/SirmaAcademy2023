3125b3a3d917b1ae5dc64e7362ae92332b16c85f
filip-mirazchiyski <124933131+filip-mirazchiyski@users.noreply.github.com>
2023-10-30 14:50:00 +0200

    Add files via upload

---
+++ BasicSyntax/BS_4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BS_4 {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+        String name = user_input.nextLine();
+        System.out.print(name);
+    }
+}
